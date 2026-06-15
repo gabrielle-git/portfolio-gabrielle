d37b81dbd232e232b99912ac347ff2430ce1473f
Gabrielle <helena.gaby31@gmail.com>
2026-06-15 11:16:54 -0300

    feat: mount visit tracker globally

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono, Pixelify_Sans } from "next/font/google";
 import "./globals.css";
 import { CommandPalette } from "@/components/ui/CommandPalette";
+import { VisitTracker } from "@/components/analytics/VisitTracker";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -65,6 +66,7 @@ export default function RootLayout({
       >
         {children}
         <CommandPalette />
+        <VisitTracker />
       </body>
     </html>
   );
